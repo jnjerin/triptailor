35b00f9ec186af8cbe130c50b419d5b7e1fd40f0
jnjerin <njerijoan24@gmail.com>
2024-06-17 20:04:25 +0300

    Added Router and ThemeProvider components

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme'; // create this file later
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+    </Router>
   </React.StrictMode>
 );
 
