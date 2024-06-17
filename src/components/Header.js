8b5038df87200de65d250fda1982028a1e4b50b4
jnjerin <njerijoan24@gmail.com>
2024-06-17 15:04:05 +0300

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+return (
+<header>
+<nav>
+<div className="logo">Triptailor</div>
+<ul>
+<li>Home</li>
+<li>Itineraries</li>
+<li>Reviews</li>
+</ul>
+</nav>
+</header>
+);
+};
+
+export default Header;
