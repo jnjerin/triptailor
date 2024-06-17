c780bbf74215174d7df4ebce38a153a60fb834d1
jnjerin <njerijoan24@gmail.com>
2024-06-17 20:08:20 +0300

    added MUI theme

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5', // Change this to your desired primary color
+    },
+    secondary: {
+      main: '#f50057', // Change this to your desired secondary color
+    },
+  },
+});
+
+export default theme;
